601356ac0351485ac759b28b3b9a6b0aa9924286
Ed <iameddie1999@gmail.com>
2022-03-30 01:38:06 -0600

    add test class

---
+++ Test.cs
@@ -0,0 +1,23 @@
+// C# program to print Hello World!
+using System;
+  
+// namespace declaration
+namespace HelloWorldApp {
+  
+  // Class declaration
+  class Geeks {
+    
+      // Main Method
+      static void Main(string[] args)
+      {
+          string uselessString = "123";
+          // statement
+          // printing Hello World!
+          Console.WriteLine("Hello World!");
+    
+          // To prevents the screen from
+          // running and closing quickly
+          Console.ReadKey();
+      }
+  }
+}
